75a35aaf7dd2fdc41a22a5a150e0c9245e5bd1cd
Layo Kaminski <layokaminski@gmail.com>
2021-03-02 18:41:54 -0300

    Resolução exercicio 9

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Users = ({ greetingMessage = 'Hi There' }) => (
-  <div>
-    <h2> Users </h2>
-    <p> {greetingMessage}, this is my awesome Users component </p>
-  </div>
-);
+class Users extends Component {
+  render() {
+    const { greetingMessage, match } = this.props;
+    const { usersId } = match.params;
+    return (
+      <div>
+        { usersId ? <h2>{ usersId }</h2> : <h2>Users</h2> }
+        <p>
+          {greetingMessage}, this is my awesome Users component
+        </p>
+      </div>
+    );
+  }
+};
 
 export default Users;
