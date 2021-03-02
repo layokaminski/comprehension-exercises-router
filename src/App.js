48d12d13f903353e4f07a2e58a1a9e93532fb1da
Layo Kaminski <layokaminski@gmail.com>
2021-03-02 15:55:00 -0300

    Resolução exercicios 5 e 6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route  path="/" component={ Home }/>
+        <Route  exact path="/" component={ Home }/>
         <Route  path="/about" component={ About }/>
         <Route  path="/users" component={ Users }/>
         <Link to="/">Home</Link><br />
