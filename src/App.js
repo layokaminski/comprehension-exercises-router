c3c5d240bbc6287b7c055c684024608eacce08f8
Layo Kaminski <layokaminski@gmail.com>
2021-03-02 19:03:16 -0300

    Resolução exercicio 10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
 
@@ -8,9 +8,14 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route  exact path="/" component={ Home }/>
-        <Route  path="/about" component={ About }/>
-        <Route  exact path="/users/:usersId" render={(props) => <Users {...props} greetingMessage="Good Morning" /> }/>
+        <Switch>
+          <Route exact path="/users/:usersId" render={(props) => (
+            <Users {...props} greetingMessage="Good Morning" />
+            )}
+          />
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
         <Link to="/">Home</Link><br />
         <Link to="/about">About</Link><br />
         <Link to="/users">Users</Link><br />
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingMessage, match } = this.props;
+    const { greetingMessage = 'Hi There', match } = this.props;
     const { usersId } = match.params;
     return (
       <div>
-        { usersId ? <h2>{ usersId }</h2> : <h2>Users</h2> }
+        <div>
+        { usersId ? <h2>{ usersId }</h2>  : <h2>Users</h2> }
+        </div>
         <p>
           {greetingMessage}, this is my awesome Users component
         </p>
